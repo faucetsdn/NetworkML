c52b79e0a64a603adefad8b8c803599ea7c836f6
Cory Stephenson <CStephenson970@gmail.com>
2017-07-13 16:03:23 -0400

    Update featurizer to use the new session format

--- TraditionalML/featurizer.py
+++ TraditionalML/featurizer.py
@@ -1,95 +1,49 @@
 import numpy as np
 from collections import defaultdict
 
-from Sessionizer import Sessionizer
-
-def get_source(hex_sessions):
+def packet_size(packet):
     '''
-    Gets the source IP address from a hex session dictionary.
-    Also computes the number of sessions to and from this source.
-    The source is defined to be the IP address with the most outgoing
-    sessions associated with it.
+    Extracts the size of a packet in bytes from the hex header.
 
-    Inputs:
-        hex_sessions: A dictionary of hex sessions from the sessionizer
-
-    Returns:
-        capture_source: Address of the capture source
-        num_incoming: # of incoming sessions to the capture source
-        num_outgoing: # of outgoing sessions from the capture source
-    '''
-
-    # Incoming sessions have the address as the destination
-    incoming_sessions = defaultdict(int)
-    # Outgoing sessions have the address as the source
-    outgoing_sessions = defaultdict(int)
-
-    # Count the incoming/outgoing sessions for all addresses
-    for key in hex_sessions:
-        incoming_address = key[1].split(':')[0]
-        outgoing_address = key[0].split(':')[0]
-
-        incoming_sessions[incoming_address] += 1
-        outgoing_sessions[outgoing_address] += 1
-
-    # The address with the most outgoing sessions is the capture source
-    if len(hex_sessions) == 0:
-        return None, 0, 0
-
-    capture_source = max(
-                          outgoing_sessions.keys(),
-                          key=(lambda k: outgoing_sessions[k])
-                        )
-
-    # Get the incoming/outgoing sessions for the capture source
-    num_incoming = incoming_sessions[capture_source]
-    num_outgoing = outgoing_sessions[capture_source]
-
-    return capture_source, num_incoming, num_outgoing
-
-def extract_packet_size(packet):
-    '''
-    Extracts the size of a packer in bytes from the hex header.
-
-    Inputs:
+    Args:
         packet: Hex header of the packet
 
-    Outputs:
-        packet_size: Size in bytes of the IP packet, including data
+    Returns:
+        size: Size in bytes of the IP packet, including data
     '''
 
-    packet_size = packet[32:36]
-    return int(packet_size, 16)
+    size = packet[1][32:36]
+    return int(size, 16)
 
 def extract_session_size(session):
     '''
     Extracts the total size of a session in bytes.
 
-    Inputs:
+    Args:
         session: session list containing all the packets of the session
 
     Returns:
         session_size: Size of the session in bytes
     '''
 
-    session_size = sum([extract_packet_size(p) for p in session])
+    session_size = sum([packet_size(p) for p in session])
     return session_size
 
 def extract_protocol(session):
     '''
     Extracts the protocol used in the session from the first packet
 
-    Inputs:
-        session: session list containing all the packets of the session
+    Args:
+        session: session tuple containing all the packets of the session
 
     Returns:
         protocol: Protocol number used in the session
     '''
 
-    protocol = session[0][46:48]
+    protocol = session[0][1][46:48]
     return protocol
 
-def extract_features(pcap_path, max_port=1024):
+def extract_features(session_dict, capture_source, max_port=1024):
     '''
     Extracts netflow level features from packet capture. Features are:
         - Number of incoming sessions
@@ -99,7 +53,7 @@ def extract_features(pcap_path, max_port=1024):
         - Number of unique ports used as source or destination
         - # of sessions, packets, and bytes sent/received on each port
 
-    Inputs:
+    Args:
         pcap_path: path to the packet capture to process into features
         max_port:  Maximum port to get features on (default 1024)
 
@@ -124,20 +78,6 @@ def extract_features(pcap_path, max_port=1024):
                                 m = 8: # of ICMP sessions on port
     '''
 
-    # Create a sessionizer for this pcap and run it
-    sessionizer = Sessionizer(pcap_path)
-    packets = sessionizer.packetizer()
-    ordered_keys = sessionizer.order_keys()
-    hex_sessions = sessionizer.hexSessionizer()
-
-    # Determine the IP address that this pcap belongs to and count the
-    # number of incoming and outgoing sessions and the ratio
-    capture_source, num_incoming, num_outgoing = get_source(hex_sessions)
-    if num_outgoing == 0:
-        io_ratio = 0
-    else:
-        io_ratio = num_incoming / num_outgoing
-
     # Compute port specific features for the capture source 
     source_ports, sent_packets, sent_bytes = [
                                               defaultdict(int),
@@ -157,15 +97,18 @@ def extract_features(pcap_path, max_port=1024):
                                                  defaultdict(int)
                                                 ]
     other_ips = defaultdict(int)
+    num_incoming = 0
+    num_outgoing = 0
 
     # Iterate over all the sessions and aggregate the port specific info
-    for key, session in hex_sessions.items():
-        address_1, port_1, *rest = key[0].split(':')
-        address_2, port_2, *rest = key[1].split(':')
+    for key, session in session_dict.items():
+        address_1, port_1 = key[0].split(':')
+        address_2, port_2 = key[1].split(':')
 
         protocol = extract_protocol(session)
 
         if address_1 == capture_source:
+            num_outgoing += 1
             if int(port_1) <= max_port:
                 source_ports[port_1] += 1
                 sent_packets[port_1] += len(session)
@@ -185,22 +128,7 @@ def extract_features(pcap_path, max_port=1024):
             other_ips[address_2] += 1
 
         if address_2 == capture_source:
-            if int(port_2) <= max_port:
-                source_ports[port_2] += 1
-                sent_packets[port_2] += len(session)
-                sent_bytes[port_2] += extract_session_size(session)
-                if protocol == '06': tcp_sessions[port_2] += 1
-                if protocol == '11': udp_sessions[port_2] += 1
-                if protocol == '01': icmp_sessions[port_2] += 1
-
-            if int(port_1) <= max_port:
-                destination_ports[port_1] += 1
-                received_packets[port_1] += len(session)
-                received_bytes[port_1] += extract_session_size(session)
-                if protocol == '06': tcp_sessions[port_1] += 1
-                if protocol == '11': udp_sessions[port_1] += 1
-                if protocol == '01': icmp_sessions[port_1] += 1
-
+            num_incoming += 1
             other_ips[address_1] += 1
 
     port_aspects = [
@@ -216,6 +144,11 @@ def extract_features(pcap_path, max_port=1024):
                    ]
 
     num_features = 6 + (max_port+1)*len(port_aspects)
+    if num_outgoing == 0:
+        io_ratio = 1
+    else:
+        io_ratio = num_incoming/num_outgoing
+
     feature_vector = np.zeros(num_features)
     feature_vector[0] = num_incoming
     feature_vector[1] = num_outgoing
