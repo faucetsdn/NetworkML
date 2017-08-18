d08f0d6c7747152b4e9f5572ff3071362be0783b
Cory Stephenson <CStephenson970@gmail.com>
2017-08-18 14:02:11 -0400

    Changes to information stored

--- TraditionalML/eval_OneLayer.py
+++ TraditionalML/eval_OneLayer.py
@@ -12,53 +12,150 @@ from redis import StrictRedis
 from OneLayer import OneLayerModel
 from featurizer import is_private
 
-def update_representation(source_ip, representations, timestamps):
+def get_previous_state(source_ip, timestamp):
     '''
-    Updates the stored representaion with the new information
+    Gets the average representation vector from the most recent update
+    before the current timestamp
 
     Args:
-        source_ip: Address of the representaion to update
-        representations: New observations of representations
-        timestamps: Time at which each representation was observed
+        source_ip: IP address to read the representation from
+        timestamp: Current timestamp.  Get only representations before this
+
+    Returns:
+        last_update: Timestamp of last update
+        previous_representation: Average representation at last update
     '''
 
-    # Set the information decay rate to 1 day
-    time_const = 60*60*24
-
-    # Read the old representation from storage. The key should be
-    # The IP address string source_ip and the value should contain the
-    # Timestamp of the last update and the previous representation vector
+    # Try to read the old updates, if there are none return Nones
     try:
         r = StrictRedis(host='redis', port=6379, db=0)
-        state = r.hgetall(source_ip)
+        updates = r.hgetall(source_ip)
     except Exception as e:
-        state = None
+        return None, None
 
-    representation = None
-    if state:
-        representation = json.loads(state[b'representation'].decode('ascii'))
-        prev_time = float(state[b'time'])
+    # Get the most recent prior timestamp from the update list
+    update_list = json.loads(updates[b'timestamps'].decode('ascii'))
+    last_update = None
+    for update in update_list:
+        if update < timestamp:
+            last_update = update
 
-    if representation is None:
-        prev_time = None
-        representation = np.zeros(representations.shape[1])
+    # Return Nones if there is no earlier update
+    if last_update is None:
+        return None, None
 
+    # Read the last updated entry to get the previous representation
+    key = source_ip + '_' + str(last_update)
+    try:
+        previous_state = r.hgetall(key)
+    except Exception as e:
+        return None, None
+    previous_representation = json.loads(
+                                       state[b'representation'].decode('ascii')
+                                        )
+    return last_update, previous_representation
+
+def average_representation(
+                            representations,
+                            timestamps,
+                            prev_representation=None,
+                            last_update=None,
+                          ):
+    '''
+    Computes the new representation from the old ones if they are given.
+    If they are not, compute the EMA of the new observations
+
+    Args:
+        representations: New representations
+        timestamps: Times that the new representations were seen
+        prev_representation: The representation computed at last_update
+        last_update: Time of previous representation update
+
+    Returns:
+        new_timestamp: Timestamp of the new representations
+        new_representation: Newly computed representations
+    '''
+
+    # Set the time constant to one day
+    time_const = 60*60*24
+
+    # If there is no previous representation, default to zeros
+    if prev_representation is None:
+        last_update = None
+        prev_representation = np.zeros(representations.shape[1])
+
+    prev_time = last_update
+    representation = prev_representation
     for i, rep in enumerate(representations):
         time = timestamps[i].timestamp()
-        if prev_time is None:
+        # If there was no previous update the representation is set equal to
+        # the current representaton
+        if  prev_time is None:
             representation = rep
             prev_time = time
+        # If the time of the representation is after the previous update, 
+        # compute the exponentially weighted moving average.
         elif time > prev_time:
             time_diff = time - prev_time
             alpha = 1 - np.exp(-time_diff/time_const)
             representation += alpha*(rep - representation)
             prev_time = time
 
-    state = {"time": time, "representation": list(representation)}
+    return time, representation
+
+def update_data(
+                 source_ip,
+                 representations,
+                 timestamps,
+                 predictions,
+                 other_ips
+               ):
+    '''
+    Updates the stored data with the new information
+
+    Args:
+        source_ip: Address of the representaion to update
+        representations: New observations of representations
+        timestamps: Time at which each representation was observed
+        predictions: Model predictions along with confidences
+        other_ips: Other IP addresses the source has communicated with
+    '''
+
     try:
-        r.hmset(source_ip, state)
+        r = StrictRedis(host='redis', port=6379, db=0)
+    except:
+        pass
+    # Get the previous update time and average representation
+    last_update, prev_rep = get_previous_state(source_ip, timestamps[0])
+
+    # Compute current representation
+    time, current_rep = average_representation(representations, timestamps)
+
+    # Compute moving average representation
+    time, avg_rep = average_representation(
+                                            representations,
+                                            timestamps,
+                                            prev_representation=prev_rep,
+                                            last_update=last_update
+                                          )
+
+    # Separate labels and confidences
+    labels= [label for label, confidence in predictions]
+    confidences = [confidence for label, confidence in predictions]
+
+    # Create the information to store
+    key = source_ip + '_' + str(time)
+    state = {
+                "representation": list(avg_rep),
+                "current_representation": list(current_rep),
+                "labels": labels,
+                "confidences": confidences,
+                "other_ips": other_ips
+            }
+    try:
+        r.hmset(key, state)
     except Exception as e:
-        print(source_ip)
+        print(key)
         print(state)
 
 if __name__ == '__main__':
@@ -68,7 +165,7 @@ if __name__ == '__main__':
     split_path = os.path.split(pcap_path)[-1]
     split_path = split_path.split('.')
     split_path = split_path[0].split('-')
-    if len(split_path) == 8:
+    if len(split_path) >= 7:
         source_ip = '.'.join(split_path[-4:])
     else:
         source_ip = None
@@ -95,4 +192,4 @@ if __name__ == '__main__':
 
         # Update the stored representation
         if reps is not None and is_private(source_ip):
-            update_representation(source_ip, reps, timestamps)
+            update_data(source_ip, reps, timestamps, preds, others)
