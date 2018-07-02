96bd185f51e4c26aec144d8629c2b9a207f7dd0e
cglewis <clewis@iqt.org>
2018-07-02 09:21:56 -0700

    more cleanup for the overhaul

--- DeviceClassifier/OneLayer/test_OneLayer.py
+++ DeviceClassifier/OneLayer/test_OneLayer.py
@@ -89,7 +89,7 @@ if __name__ =='__main__':
     if len(sys.argv) >= 4:
         save_path = sys.argv[3]
     else:
-        save_path = "/models/OneLayerModel.pkl"
+        save_path = "models/OneLayerModel.pkl"
     model = OneLayerModel(duration=None, hidden_size=None)
     logger.info("Loading model from %s", load_path)
     model.load(load_path)
