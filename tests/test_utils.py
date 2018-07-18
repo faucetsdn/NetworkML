674cebd7f99297d830091c1d1f6509c208be519c
cglewis <clewis@iqt.org>
2018-07-17 21:01:58 -0700

    increase test coverage

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,4 +1,16 @@
 from utils.OneLayer import OneLayerModel
+from utils.RandomForestModel import RandomForestModel
+from utils.SoSmodel import scope_decorator
+from utils.SoSmodel import get_available_gpus
+from utils.SoSmodel import weight_variable
+from utils.SoSmodel import bias_variable
+from utils.SoSmodel import SoSModel
 
 def test_OneLayerModel():
     instance = OneLayerModel(15)
+
+def test_RandomForestModel():
+    instance = RandomForestModel(15)
+
+def test_SoSModel():
+    instance = SoSModel()
