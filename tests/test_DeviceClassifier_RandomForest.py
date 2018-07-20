72ec88797c3d2b0ab8e2c18d21e1df3e9998b0e3
cglewis <clewis@iqt.org>
2018-07-20 08:31:11 -0700

    more tests

--- tests/test_DeviceClassifier_RandomForest.py
+++ tests/test_DeviceClassifier_RandomForest.py
@@ -9,3 +9,15 @@ from DeviceClassifier.RandomForest.test_RandomForest import calc_f1
 
 def test_calc_f1():
     calc_f1({})
+
+def test_lookup_key():
+    address, e = lookup_key('foo')
+    assert address == None
+
+def test_get_address_info():
+    current_state, average_state, other_ips, last_update, labels, conf = get_address_info('10.0.0.1', '1', 1)
+
+def test_get_previous_state():
+    last_update, previous_representation = get_previous_state('10.0.0.1', '1')
+    assert last_update == None
+    assert previous_representation == None
