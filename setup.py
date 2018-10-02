25cea16694f8e7002b7ee6e47f1d1f8cbbc98e17
Charlie Lewis <clewis@iqt.org>
2018-10-02 08:15:09 -0400

    update libs and model

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     package_dir={'poseidonml': 'utils'},
     package_data={'poseidonml': ['models/*']},
     install_requires=['numpy==1.15.1', 'pika==0.12.0', 'redis==2.10.6',
-                      'scikit-learn==0.19.2', 'scipy==1.1.0', 'tensorflow==1.10.1'],
+                      'scikit-learn==0.20.0', 'scipy==1.1.0', 'tensorflow==1.11.0'],
     license='Apache License 2.0',
     author='cglewis',
     author_email='clewis@iqt.org',
