18bb1ae0c797e8eac090c7de58893fc3631c3e12
Ryan Ashley <rashley@iqt.org>
2018-10-10 22:44:42 -0400

    fixed typo

--- utils/config.py
+++ utils/config.py
@@ -10,7 +10,7 @@ logging.basicConfig(level=logging.INFO)
 def get_config(cfg_file='opts/config.json'):
 	logger = logging.getLogger(__name__)
 	try:
-		with open('opts/config.json', 'r') as config_file:
+		with open(cfg_file, 'r') as config_file:
 			config = json.load(config_file)
 			return config
 	except Exception as e:  # pragma: no cover
