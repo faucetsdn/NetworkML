4f8b8d942df3a6a01b9ed9d52995fc0096a4a039
cglewis <clewis@iqt.org>
2018-10-22 06:01:20 -0700

    release v0.2.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='poseidonml',
-    version='0.2.2.dev',
+    version='0.2.2',
     packages=['poseidonml'],
     package_dir={'poseidonml': 'utils'},
     package_data={'poseidonml': ['models/*']},
