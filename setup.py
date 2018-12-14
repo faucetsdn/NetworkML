593c0e0b414415ea9f0772edb5f84b3d6c758bb6
cglewis <clewis@iqt.org>
2018-12-14 10:19:20 -0800

    revert

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='poseidonml',
-    version='0.2.4.dev',
+    version='0.2.3',
     packages=['poseidonml'],
     package_dir={'poseidonml': 'utils'},
     package_data={'poseidonml': ['models/*']},
