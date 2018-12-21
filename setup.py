495d67e4c41f9ca66249e0fdd352bd39f3e5a7e0
lilchurro <lilchurro@users.noreply.github.com>
2018-12-21 11:03:21 -0500

    Moving to new dev version.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='poseidonml',
-    version='0.2.4',
+    version='0.2.5.dev',
     packages=['poseidonml'],
     package_dir={'poseidonml': 'utils'},
     package_data={'poseidonml': ['models/*']},
