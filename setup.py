1a1ddcd86a5325ed3b7e122495eab0f487a1c917
lilchurro <lilchurro@users.noreply.github.com>
2018-12-21 10:50:18 -0500

    Releasing version 0.2.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='poseidonml',
-    version='0.2.4.dev',
+    version='0.2.4',
     packages=['poseidonml'],
     package_dir={'poseidonml': 'utils'},
     package_data={'poseidonml': ['models/*']},
@@ -11,8 +11,8 @@ setup(
     license='Apache License 2.0',
     author='cglewis',
     author_email='clewis@iqt.org',
-    maintainer='Charlie Lewis',
-    maintainer_email='clewis@iqt.org',
+    maintainer='Alice Chang',
+    maintainer_email='achang@iqt.org',
     description=(
         'A utility package for extracting and analyzing features in network traffic.'),
     keywords='machine learning network analysis utilities',
