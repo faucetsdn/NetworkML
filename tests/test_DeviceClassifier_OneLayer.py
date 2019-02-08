445b7f3c80ee3bab249abfefd5212ab0afd9e036
Alice <lilchurro@users.noreply.github.com>
2019-02-08 16:48:06 -0500

    Enhance/183 (#294)
    * Modified train_OneLayer for command-line usage.
    
    * Modified train_RandomForest for command-line usage.
    
    * Added the ability for the test scripts in OneLayer and RandomForest to be
    run from commandline/interpreter with some flags (run -h to see help).
    To note: The model paths are different between docker-run and CLI -- when
    reading in the model, docker containers look for /model, which obvs
    doesn't work with CLI. Will need to figure out a solution for this later.
    
    * Modified the filenames of the output from the test scripts to be [model].json,
    as they were messing up the existing pkl files. Haven't transitioned to saving
    and loading the models via json file just yet, but it's in the pipeline.
    
    * Fixed some tests to accommodate the new code changes.
    
    * Removed unused modules from test scripts.
    
    * Modified some paths to incorporate test_labels.json input file into the testing process.

--- tests/test_DeviceClassifier_OneLayer.py
+++ tests/test_DeviceClassifier_OneLayer.py
@@ -3,15 +3,10 @@ import os
 
 from DeviceClassifier.OneLayer import train_OneLayer
 from DeviceClassifier.OneLayer.eval_OneLayer import OneLayerEval
-from DeviceClassifier.OneLayer.test_OneLayer import calc_f1
 
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
-
-
-def test_calc_f1():
-    calc_f1({}, logger)
 
 
 def test_onelayereval():
