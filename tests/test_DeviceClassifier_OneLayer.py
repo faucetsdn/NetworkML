38561c04ef19ffa492749fc0cf0499a3bf774e7a
cglewis <clewis@iqt.org>
2019-02-22 13:25:54 -0800

    add test coverage

--- tests/test_DeviceClassifier_OneLayer.py
+++ tests/test_DeviceClassifier_OneLayer.py
@@ -12,10 +12,19 @@ logger = logging.getLogger(__name__)
 def test_onelayereval():
     os.environ['SKIP_RABBIT'] = 'True'
     instance = OneLayerEval()
-    # instance.main()
 
     last_update = instance.common.get_address_info('10.0.0.1', '1')
     last_update, previous_representation = instance.common.get_previous_state(
         '10.0.0.1', '1')
     assert last_update == None
     assert previous_representation == None
+
+
+def test_onelayereval_main():
+    os.environ['SKIP_RABBIT'] = 'True'
+    instance = OneLayerEval()
+
+    sys.argv = ['foo', '.']
+    instance.main()
+    sys.argv = ['foo', 'AUTHORS']
+    instance.main()
