9c698a8b7a3965fe8e4982759bb7dd3879e21c5f
cglewis <clewis@iqt.org>
2019-03-12 09:26:51 -0700

    create labels array with unknown

--- tests/test_utils_model.py
+++ tests/test_utils_model.py
@@ -4,9 +4,9 @@ from utils.Model import Model
 
 
 def test_augment_data():
-    model = Model(10)
+    model = Model(10, labels=['Unknown'])
     a = [[1, 2, 3], [4, 5, 6]]
     x = np.array(a)
-    a = ['label1', 'label2', 'Unknown']
+    a = ['label1', 'label2', 'label3']
     y = np.array(a)
     model._augment_data(x, y)
