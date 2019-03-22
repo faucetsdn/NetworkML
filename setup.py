e207e099548825b81bb35ce0d008b62b5605b10d
cglewis <clewis@iqt.org>
2019-03-22 08:10:44 -0700

    bump back to dev

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='poseidonml',
-    version='0.2.10',
+    version='0.3.0.dev',
     packages=['poseidonml'],
     package_dir={'poseidonml': 'utils'},
     package_data={'poseidonml': ['models/*']},
