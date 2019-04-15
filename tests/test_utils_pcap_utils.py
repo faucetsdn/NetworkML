28aafc3a6d95ebdbbd068b1c92cfa71591ce0204
cglewis <clewis@iqt.org>
2019-04-15 14:27:25 -0700

    more tests

--- tests/test_utils_pcap_utils.py
+++ tests/test_utils_pcap_utils.py
@@ -1,7 +1,33 @@
 from networkml.parsers.pcap.pcap_utils import extract_macs
+from networkml.parsers.pcap.pcap_utils import is_private
 
 
 def test_extract_macs():
     source, dest = extract_macs('123456789ABCDEF123456780')
     assert dest == '12:34:56:78:9A:BC'
     assert source == 'DE:F1:23:45:67:80'
+
+
+def test_is_private():
+    private = is_private('10.10.10.10')
+    assert private == True
+    private = is_private('1.2.3.4')
+    assert private == False
+    private = is_private('192.168.1.1')
+    assert private == True
+    private = is_private('192.169.1.1')
+    assert private == False
+    private = is_private('172.16.4.4')
+    assert private == True
+    private = is_private('172.15.1.3')
+    assert private == False
+    private = is_private('172.32.3.1')
+    assert private == False
+    private = is_private('2014::1')
+    assert private == False
+    private = is_private('fe80::1')
+    assert private == True
+    private = is_private('fd13::13')
+    assert private == True
+    private = is_private('asdf')
+    assert private == False
