5a4b5b008a17e7cb763770f613323007a520cddd
cglewis <clewis@iqt.org>
2019-04-15 11:51:50 -0700

    argparse protects from else options

--- tests/test_networkml.py
+++ tests/test_networkml.py
@@ -3,6 +3,11 @@ import sys
 from networkml.NetworkML import NetworkML
 
 
+def test_networml_nofiles():
+    sys.argv = ['bin/networkml']
+    netml = NetworkML()
+
+
 def test_networkml_eval_onelayer():
     sys.argv = ['bin/networkml', '-p', 'tests/']
     netml = NetworkML()
@@ -15,11 +20,6 @@ def test_networkml_eval_randomforest():
 
 def test_networkml_eval_sos():
     sys.argv = ['bin/networkml', '-p', 'tests/', '-a', 'sos']
-    netml = NetworkML()
-
-
-def test_networkml_eval_bad():
-    sys.argv = ['bin/networkml', '-p', 'tests/', '-a', 'bad']
     netml = NetworkML()
 
 
@@ -39,11 +39,6 @@ def test_networkml_train_sos():
     netml = NetworkML()
 
 
-def test_networkml_train_bad():
-    sys.argv = ['bin/networkml', '-p', 'tests/', '-o', 'train', '-a', 'bad']
-    netml = NetworkML()
-
-
 def test_networkml_test_onelayer():
     sys.argv = ['bin/networkml', '-p', 'tests/', '-o', 'test']
     netml = NetworkML()
@@ -56,16 +51,6 @@ def test_networkml_test_randomforest():
 
 
 def test_networkml_test_sos():
-    sys.argv = ['bin/networkml', '-p', 'tests/', '-o', 'test', '-a', 'sos']
-    netml = NetworkML()
-
-
-def test_networkml_test_bad():
     sys.argv = ['bin/networkml', '-p',
-                'tests/test.pcap', '-o', 'test', '-a', 'bad']
-    netml = NetworkML()
-
-
-def test_networkml_bad():
-    sys.argv = ['bin/networkml', '-o', 'bad']
+                'tests/test.pcap', '-o', 'test', '-a', 'sos']
     netml = NetworkML()
