ff69a74e14b767274e5228fe0fe6847d230c66f8
Josh Bailey <josh@vandervecken.com>
2019-12-18 17:47:25 +1300

    auto is too much CPU for travis.

--- travis-tests.sh
+++ travis-tests.sh
@@ -4,7 +4,7 @@ set -e
 
 case ${TESTTYPE} in
   UNIT)
-    pytype . && pytest -l -s -n auto -v --cov=tests/ --cov=networkml/ --cov-report term-missing -c .coveragerc && coverage
+    pytype . && pytest -l -s -n 2 -v --cov=tests/ --cov=networkml/ --cov-report term-missing -c .coveragerc && coverage
     ;;
   DOCKER)
     make test
