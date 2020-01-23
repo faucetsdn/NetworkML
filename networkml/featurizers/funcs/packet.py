c607d522215d1cf0049117a8bdffe802ea915831
cglewis <clewis@iqt.org>
2020-01-22 16:34:16 -0800

    fix pyshark

---
+++ networkml/featurizers/funcs/packet.py
@@ -0,0 +1,9 @@
+from networkml.featurizers.features import Features
+
+class Packet(Features):
+
+    def default_ok(self, file_input):
+        print(f'running default, ok {file_input}')
+
+    def transform_ok(self, file_input):
+        print(f'it is ok {file_input}')
