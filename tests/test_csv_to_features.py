25f0b9ec8e1e29d127be0565ca4233fb52238ec8
cglewis <clewis@iqt.org>
2020-01-27 21:20:21 -0800

    more tests

--- tests/test_csv_to_features.py
+++ tests/test_csv_to_features.py
@@ -8,6 +8,16 @@ def test_CSVToFeatures():
     sys.argv = ['pcap_to_csv.py', '-e', 'tshark', './tests/trace_ab12_2001-01-01_02_03-client-ip-1-2-3-4.pcap']
     instance = PCAPToCSV()
     instance.main()
-    sys.argv = ['csv_to_features.py', '-g', 'tshark', './tests/trace_ab12_2001-01-01_02_03-client-ip-1-2-3-4.pcap.csv.gz']
+    sys.argv = ['csv_to_features.py', '-c', '-g', 'tshark', '-o', '/tmp/combined.csv.gz', './tests/trace_ab12_2001-01-01_02_03-client-ip-1-2-3-4.pcap.csv.gz']
     instance2 = CSVToFeatures()
     instance2.main()
+
+
+def test_CSVToFeatures_dir():
+    sys.argv = ['pcap_to_csv.py', '-e', 'tshark', '-o', '/tmp/foo', './tests']
+    instance = PCAPToCSV()
+    instance.main()
+    sys.argv = ['csv_to_features.py', '-c', '-g', 'tshark', '/tmp/foo']
+    instance2 = CSVToFeatures()
+    instance2.main()
+
