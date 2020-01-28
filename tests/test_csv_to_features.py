75b30ddc35df002045125ed152ce2e1988e263ae
cglewis <clewis@iqt.org>
2020-01-27 19:10:27 -0800

    more tests

--- tests/test_csv_to_features.py
+++ tests/test_csv_to_features.py
@@ -5,9 +5,9 @@ from networkml.parsers.pcap_to_csv import PCAPToCSV
 
 
 def test_CSVToFeatures():
-    sys.argv = ['pcap_to_csv.py', './tests/trace_ab12_2001-01-01_02_03-client-ip-1-2-3-4.pcap']
+    sys.argv = ['pcap_to_csv.py', '-e', 'tshark', './tests/trace_ab12_2001-01-01_02_03-client-ip-1-2-3-4.pcap']
     instance = PCAPToCSV()
     instance.main()
-    sys.argv = ['csv_to_features.py', './tests/trace_ab12_2001-01-01_02_03-client-ip-1-2-3-4.pcap.csv.gz']
+    sys.argv = ['csv_to_features.py', '-g', 'tshark', './tests/trace_ab12_2001-01-01_02_03-client-ip-1-2-3-4.pcap.csv.gz']
     instance2 = CSVToFeatures()
     instance2.main()
--- tests/test_pcap_to_csv.py
+++ tests/test_pcap_to_csv.py
@@ -4,7 +4,7 @@ from networkml.parsers.pcap_to_csv import PCAPToCSV
 
 
 def test_PCAPToCSV():
-    sys.argv = ['pcap_to_csv.py', '-c', '-t', '2', '-o', '/tmp/networkml_test.pcap.csv.gz', './tests/trace_ab12_2001-01-01_02_03-client-ip-1-2-3-4.pcap']
+    sys.argv = ['pcap_to_csv.py', '-c', '-e', 'pyshark', '-t', '2', '-o', '/tmp/networkml_test.pcap.csv.gz', './tests/trace_ab12_2001-01-01_02_03-client-ip-1-2-3-4.pcap']
     instance = PCAPToCSV()
     instance.main()
 
