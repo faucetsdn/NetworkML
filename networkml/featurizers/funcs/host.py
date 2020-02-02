b8c80792b63a8fa455322068a8ea4f89e1b825b5
Josh Bailey <josh@vandervecken.com>
2020-02-03 12:30:44 +1300

    More frame lengths.

--- networkml/featurizers/funcs/host.py
+++ networkml/featurizers/funcs/host.py
@@ -14,20 +14,23 @@ class Host(Features):
         'variance': statistics.variance,
         '25q': lambda x: percentile(x, 25),
         '75q': lambda x: percentile(x, 75),
+        'total': sum,
     }
 
 
     def _calc_tshark_field(self, field, tshark_field, rows):
-        field_prefix = field.split('_')[0]
-        field_suffix = field.split('_')[-1]
+        field_parts = field.split('_')
+        field_prefix = field_parts[0]
+        field_suffix = field_parts[-1]
         stat = self.NAME_TO_STAT.get(field_prefix, None)
         assert stat is not None
-        row_filter = rows
+        rows_filter = rows
         if field_suffix == 'in':
             rows_filter = self._select_mac_direction(rows, output=False)
-        elif field_suffix = 'out':
+        elif field_suffix == 'out':
             rows_filter = self._select_mac_direction(rows, output=True)
-        return [{field: self._stat_row_field(stat, tshark_field, rows_filter)}]
+        new_rows = [{field: self._stat_row_field(stat, tshark_field, rows_filter)}]
+        return new_rows
 
 
     def _pyshark_ipversions(self, rows):
@@ -91,6 +94,7 @@ class Host(Features):
             return [{'IPv6': 1}]
         return [{'IPv6': 0}]
 
+    # Directionless.
 
     def tshark_avg_time_delta(self, rows):
         return self._calc_tshark_field('average_time_delta', 'frame.time_delta_displayed', rows)
@@ -131,6 +135,15 @@ class Host(Features):
     def tshark_75q_frame_len(self, rows):
         return self._calc_tshark_field('75q_frame_len', 'frame.len', rows)
 
+    # By direction
+
+    def tshark_total_frame_len_in(self, rows):
+        return self._calc_tshark_field('total_frame_len_in', 'frame.len', rows)
+
+
+    def tshark_total_frame_len_out(self, rows):
+        return self._calc_tshark_field('total_frame_len_out', 'frame.len', rows)
+
 
     def tshark_avg_frame_len_in(self, rows):
         return self._calc_tshark_field('average_frame_len_in', 'frame.len', rows)
@@ -138,3 +151,51 @@ class Host(Features):
 
     def tshark_avg_frame_len_out(self, rows):
         return self._calc_tshark_field('average_frame_len_out', 'frame.len', rows)
+
+
+    def tshark_25q_frame_len_in(self, rows):
+        return self._calc_tshark_field('25q_frame_len_in', 'frame.len', rows)
+
+
+    def tshark_25q_frame_len_out(self, rows):
+        return self._calc_tshark_field('25q_frame_len_out', 'frame.len', rows)
+
+
+    def tshark_75q_frame_len_in(self, rows):
+        return self._calc_tshark_field('75q_frame_len_in', 'frame.len', rows)
+
+
+    def tshark_75q_frame_len_out(self, rows):
+        return self._calc_tshark_field('75q_frame_len_out', 'frame.len', rows)
+
+
+    def tshark_median_frame_len_in(self, rows):
+        return self._calc_tshark_field('median_frame_len_in', 'frame.len', rows)
+
+
+    def tshark_median_frame_len_out(self, rows):
+        return self._calc_tshark_field('median_frame_len_out', 'frame.len', rows)
+
+
+    def tshark_variance_frame_len_in(self, rows):
+        return self._calc_tshark_field('variance_frame_len_in', 'frame.len', rows)
+
+
+    def tshark_variance_frame_len_out(self, rows):
+        return self._calc_tshark_field('variance_frame_len_out', 'frame.len', rows)
+
+
+    def tshark_max_frame_len_in(self, rows):
+        return self._calc_tshark_field('max_frame_len_in', 'frame.len', rows)
+
+
+    def tshark_max_frame_len_out(self, rows):
+        return self._calc_tshark_field('max_frame_len_out', 'frame.len', rows)
+
+
+    def tshark_min_frame_len_in(self, rows):
+        return self._calc_tshark_field('min_frame_len_in', 'frame.len', rows)
+
+
+    def tshark_min_frame_len_out(self, rows):
+        return self._calc_tshark_field('min_frame_len_out', 'frame.len', rows)
