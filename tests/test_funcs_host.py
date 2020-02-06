9a28ef8ac1fa1a01a547940f0bdeb6c727869205
Josh Bailey <josh@vandervecken.com>
2020-02-07 10:17:23 +1300

    Can't return empty dict.

--- tests/test_funcs_host.py
+++ tests/test_funcs_host.py
@@ -5,4 +5,5 @@ def test_protocols():
     instance = Host()
     assert instance.tshark_last_protocols_array(
         [{'frame.protocols': 'eth:ethertype:ip:udp:db-lsp-disc:json'}]) == [
-                {'protocol_db-lsp-disc': 1, 'protocol_ip': 1, 'protocol_json': 1, 'protocol_udp': 1}]
+                {'protocol_db-lsp-disc': 1, 'protocol_ip': 1, 'protocol_json': 1, 'protocol_udp': 1, 'protocol_eth': 1}]
+    assert instance.tshark_last_protocols_array([{}]) == [{'protocol_eth': 1}]
