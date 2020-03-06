ce59878c76cd6ff3acc4eb6c4779eeca3aa03195
Josh Bailey <josh@vandervecken.com>
2020-03-06 16:30:22 +1300

    Need temp dirs to be separate.

--- tests/test_csv_to_features.py
+++ tests/test_csv_to_features.py
@@ -1,16 +1,21 @@
 import sys
+import tempfile
+import os
 
 from networkml.featurizers.csv_to_features import CSVToFeatures
 from networkml.parsers.pcap_to_csv import PCAPToCSV
 
 
 def test_CSVToFeatures():
-    sys.argv = ['pcap_to_csv.py', '-e', 'tshark', '-o', '/tmp/foo-1.csv.gz', './tests/test_data/trace_ab12_2001-01-01_02_03-client-ip-1-2-3-4.pcap']
-    instance = PCAPToCSV()
-    instance.main()
-    sys.argv = ['csv_to_features.py', '-c', '-g', 'tshark', '-o', '/tmp/combined.csv.gz', '/tmp/foo-1.csv.gz']
-    instance2 = CSVToFeatures()
-    instance2.main()
+    with tempfile.TemporaryDirectory() as tmpdir:
+        sys.argv = ['pcap_to_csv.py', '-e', 'tshark', '-o', os.path.join(tmpdir, 'foo-1.csv.gz'),
+                    './tests/test_data/trace_ab12_2001-01-01_02_03-client-ip-1-2-3-4.pcap']
+        instance = PCAPToCSV()
+        instance.main()
+        sys.argv = ['csv_to_features.py', '-c', '-g', 'tshark',
+                    '-o', os.path.join(tmpdir, 'combined.csv.gz'), os.path.join(tmpdir, 'foo-1.csv.gz')]
+        instance2 = CSVToFeatures()
+        instance2.main()
 
 
 def test_CSVToFeatures_no_output():
@@ -29,36 +34,44 @@ def test_CSVToFeatures_no_group_or_func():
 
 
 def test_CSVToFeatures_dir():
-    sys.argv = ['pcap_to_csv.py', '-e', 'tshark', '-o', '/tmp/foo2', './tests']
-    instance = PCAPToCSV()
-    instance.main()
-    sys.argv = ['csv_to_features.py', '-t', '2', '-c', '-g', 'tshark', '/tmp/foo2']
-    instance2 = CSVToFeatures()
-    instance2.main()
+    with tempfile.TemporaryDirectory() as tmpdir:
+        sys.argv = ['pcap_to_csv.py', '-e', 'tshark', '-o', os.path.join(tmpdir, 'foo2'), './tests']
+        instance = PCAPToCSV()
+        instance.main()
+        sys.argv = ['csv_to_features.py', '-t', '2', '-c', '-g', 'tshark', os.path.join(tmpdir, 'foo2')]
+        instance2 = CSVToFeatures()
+        instance2.main()
 
 
 def test_CSVToFeatures_dir_output():
-    sys.argv = ['pcap_to_csv.py', '-e', 'tshark', '-o', '/tmp/foo2', './tests']
-    instance = PCAPToCSV()
-    instance.main()
-    sys.argv = ['csv_to_features.py', '-t', '2', '-c', '-g', 'tshark', '-o', '/tmp/foo2_output', '/tmp/foo2']
-    instance2 = CSVToFeatures()
-    instance2.main()
+    with tempfile.TemporaryDirectory() as tmpdir:
+        foo2 = os.path.join(tmpdir, 'foo2')
+        foo2out = os.path.join(tmpdir, 'foo2_output')
+        sys.argv = ['pcap_to_csv.py', '-e', 'tshark', '-o', foo2, './tests']
+        instance = PCAPToCSV()
+        instance.main()
+        sys.argv = ['csv_to_features.py', '-t', '2', '-c', '-g', 'tshark', '-o', foo2out, foo2]
+        instance2 = CSVToFeatures()
+        instance2.main()
 
 
 def test_CSVToFeatures_generic():
-    sys.argv = ['pcap_to_csv.py', '-e', 'tshark', '-o', '/tmp/foo1', './tests']
-    instance = PCAPToCSV()
-    instance.main()
-    sys.argv = ['csv_to_features.py', '-z', 'input', '-f', 'Generic:all', '-g', 'None', '/tmp/foo1']
-    instance2 = CSVToFeatures()
-    instance2.main()
+    with tempfile.TemporaryDirectory() as tmpdir:
+        foo1 = os.path.join(tmpdir, 'foo1')
+        sys.argv = ['pcap_to_csv.py', '-e', 'tshark', '-o', foo1, './tests']
+        instance = PCAPToCSV()
+        instance.main()
+        sys.argv = ['csv_to_features.py', '-z', 'input', '-f', 'Generic:all', '-g', 'None', foo1]
+        instance2 = CSVToFeatures()
+        instance2.main()
 
 
 def test_CSVToFeatures_host():
-    sys.argv = ['pcap_to_csv.py', '-e', 'pyshark', '-o', '/tmp/foo3', './tests']
-    instance = PCAPToCSV()
-    instance.main()
-    sys.argv = ['csv_to_features.py', '-c', '-z', 'input', '-g', 'pyshark', '/tmp/foo3']
-    instance2 = CSVToFeatures()
-    instance2.main()
+    with tempfile.TemporaryDirectory() as tmpdir:
+        foo3 = os.path.join(tmpdir, 'foo3')
+        sys.argv = ['pcap_to_csv.py', '-e', 'pyshark', '-o', foo3, './tests']
+        instance = PCAPToCSV()
+        instance.main()
+        sys.argv = ['csv_to_features.py', '-c', '-z', 'input', '-g', 'pyshark', foo3]
+        instance2 = CSVToFeatures()
+        instance2.main()
