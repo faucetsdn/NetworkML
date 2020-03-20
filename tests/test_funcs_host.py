a414ecb5d1ec22bd6c589e7d38cabb581741786e
Josh Bailey <josh@vandervecken.com>
2020-03-20 16:00:35 +1300

    Rename methods.

--- tests/test_funcs_host.py
+++ tests/test_funcs_host.py
@@ -3,19 +3,19 @@ from networkml.featurizers.funcs.host import Host
 TEST_MAC = '0e:02:03:04:05:06'
 
 
-def test_pyshark_ipv4():
+def test_host_pyshark_ipv4():
     instance = Host()
     assert instance.pyshark_ipv4(
         lambda: [{'eth.src': TEST_MAC, 'layers': {'<IP Layer>': {}}}]) == [{'IPv4': 1}]
 
 
-def test_pyshark_ipv6():
+def test_host_pyshark_ipv6():
     instance = Host()
     assert instance.pyshark_ipv6(
         lambda: [{'eth.src': TEST_MAC, 'layers': {'<IPV6 Layer>': {}}}]) == [{'IPv6': 1}]
 
 
-def test_tshark_input_mac():
+def test_host_tshark_input_mac():
     instance = Host()
     # 1 appears the most on both sides.
     # pytype: disable=attribute-error
@@ -23,7 +23,7 @@ def test_tshark_input_mac():
         lambda: [{'eth.src': 1, 'eth.dst': 2}, {'eth.src': 2, 'eth.dst': 1}, {'eth.src': 1, 'eth.dst': 99}]) == (1, {1, 2, 99})
 
 
-def test_select_mac_direction():
+def test_host_select_mac_direction():
     rows = lambda: [{'eth.src': 1, 'eth.dst': 2}, {'eth.src': 2, 'eth.dst': 1}, {'eth.src': 1, 'eth.dst': 99}]
     instance = Host()
     # pytype: disable=attribute-error
@@ -31,20 +31,20 @@ def test_select_mac_direction():
     assert [{'eth.dst': 2, 'eth.src': 1}, {'eth.dst': 99, 'eth.src': 1}] == list(instance._select_mac_direction(rows, output=True))
 
 
-def test_max_frame_time():
+def test_host_max_frame_time():
     instance = Host()
     assert instance.host_tshark_max_frame_time_in(
         lambda: [{'eth.src': TEST_MAC, 'frame.time_epoch': 999}, {'eth.src': TEST_MAC, 'frame.time_epoch': 1001}]) == [
             {'host_key': TEST_MAC, 'max_frame_time_in': 1001}]
 
 
-def test_max_frame_len():
+def test_host_max_frame_len():
     instance = Host()
     assert instance.host_tshark_max_frame_len(
         lambda: [{'eth.src': TEST_MAC, 'frame.len': 999}]) == [{'host_key': TEST_MAC, 'max_frame_len': 999}]
 
 
-def test_ipversions():
+def test_host_ipversions():
     instance = Host()
     assert instance.host_tshark_ipv4(
         lambda: [{'eth.src': TEST_MAC, 'ip.version': 4}]) == [{'host_key': TEST_MAC, 'IPv4': 1}]
@@ -52,7 +52,7 @@ def test_ipversions():
         lambda: [{'eth.src': TEST_MAC, 'ip.version': 6}]) == [{'host_key': TEST_MAC, 'IPv6': 1}]
 
 
-def test_protocols():
+def test_host_protocols():
     instance = Host()
     assert instance.host_tshark_last_protocols_array(
         lambda: [{'eth.src': TEST_MAC, 'frame.protocols': 'eth:ethertype:ip:udp:db-lsp-disc:json'}]) == [
@@ -62,7 +62,7 @@ def test_protocols():
             {'host_key': TEST_MAC, 'protocol_esp': 0, 'protocol_eth': 0, 'protocol_ip': 0, 'protocol_gre': 0, 'protocol_ipv6': 0, 'protocol_tcp': 0, 'protocol_arp': 0, 'protocol_icmp': 0, 'other': 0}]
 
 
-def test_non_ip():
+def test_host_non_ip():
     instance = Host()
     assert instance.host_tshark_non_ip(
         lambda: [{'eth.src': TEST_MAC, 'eth.type': 99}]) == [{'host_key': TEST_MAC, 'tshark_non_ip': 1}]
@@ -70,13 +70,13 @@ def test_non_ip():
         lambda: [{'eth.src': TEST_MAC, 'eth.type': 0x00000800}]) == [{'host_key': TEST_MAC, 'tshark_non_ip': 0}]
 
 
-def test_vlan_id():
+def test_host_vlan_id():
     instance = Host()
     assert instance.host_tshark_vlan_id(
         lambda: [{'eth.src': TEST_MAC, 'vlan.id': 999}]) == [{'host_key': TEST_MAC, 'tshark_vlan_id': 999}]
 
 
-def test_ipx():
+def test_host_ipx():
     instance = Host()
     assert instance.host_tshark_ipx(
         lambda: [{'eth.src': TEST_MAC, 'eth.type': 0x8137}]) == [{'host_key': TEST_MAC, 'tshark_ipx': 1}]
@@ -84,7 +84,7 @@ def test_ipx():
         lambda: [{'eth.src': TEST_MAC, 'eth.type': 0x800}]) == [{'host_key': TEST_MAC, 'tshark_ipx': 0}]
 
 
-def test_both_private_ip():
+def test_host_both_private_ip():
     instance = Host()
     assert instance.host_tshark_both_private_ip(
         lambda: [{'eth.src': TEST_MAC, 'ip.src': '192.168.0.1', 'ip.dst': '10.10.1.1'}]) == [
@@ -94,7 +94,7 @@ def test_both_private_ip():
             {'host_key': TEST_MAC, 'tshark_both_private_ip': 0}]
 
 
-def test_ipv4_multicast():
+def test_host_ipv4_multicast():
     instance = Host()
     assert instance.host_tshark_ipv4_multicast(
         lambda: [{'eth.src': TEST_MAC, 'ip.src': '192.168.0.1', 'ip.dst': '224.0.0.1'}]) == [
@@ -104,7 +104,7 @@ def test_ipv4_multicast():
             {'host_key': TEST_MAC, 'tshark_ipv4_multicast': 0}]
 
 
-def test_tcp_flags():
+def test_host_tcp_flags():
     instance = Host()
     rows = lambda: [{'eth.src': TEST_MAC, 'tcp.flags': 0x00000014}]
     assert instance.host_tshark_tcp_flags_out(rows) == [
@@ -113,7 +113,7 @@ def test_tcp_flags():
         {'host_key': TEST_MAC, 'tshark_tcp_flags_ack_in': 1, 'tshark_tcp_flags_cwr_in': 0, 'tshark_tcp_flags_ece_in': 0,  'tshark_tcp_flags_fin_in': 0, 'tshark_tcp_flags_ns_in': 0, 'tshark_tcp_flags_psh_in': 0, 'tshark_tcp_flags_rst_in': 1, 'tshark_tcp_flags_syn_in': 0, 'tshark_tcp_flags_urg_in': 0}]
 
 
-def test_ip_flags():
+def test_host_ip_flags():
     instance = Host()
     rows = lambda: [{'eth.src': TEST_MAC, 'ip.flags': 0x00004000}]
     assert instance.host_tshark_ip_flags_out(rows) == [
@@ -122,7 +122,7 @@ def test_ip_flags():
         {'host_key': TEST_MAC, 'tshark_ip_flags_rb_in': 0, 'tshark_ip_flags_df_in': 1, 'tshark_ip_flags_mf_in': 0}]
 
 
-def test_ip_dsfield_flags():
+def test_host_ip_dsfield_flags():
     instance = Host()
     rows = lambda: [{'eth.src': TEST_MAC, 'ip.dsfield': 0x000000c0}]
     assert instance.host_tshark_ip_dsfield_out(rows) == [
@@ -131,7 +131,7 @@ def test_ip_dsfield_flags():
         {'host_key': TEST_MAC, 'tshark_ip_dsfield_ecn0_in': 0, 'tshark_ip_dsfield_ecn1_in': 0, 'tshark_ip_dsfield_dscp0_in': 0, 'tshark_ip_dsfield_dscp1_in': 0, 'tshark_ip_dsfield_dscp2_in': 0, 'tshark_ip_dsfield_dscp3_in': 0, 'tshark_ip_dsfield_dscp4_in': 1, 'tshark_ip_dsfield_dscp5_in': 1}]
 
 
-def test_tcp_priv_ports():
+def test_host_tcp_priv_ports():
     instance = Host()
     rows = lambda: [
         {'eth.src': TEST_MAC, 'tcp.srcport': 1025, 'tcp.dstport': 80},
@@ -143,7 +143,7 @@ def test_tcp_priv_ports():
         {'host_key': TEST_MAC, 'tshark_tcp_priv_port_161_out': 0, 'tshark_tcp_priv_port_67_out': 0, 'tshark_tcp_priv_port_68_out': 0, 'tshark_tcp_priv_port_69_out': 0, 'tshark_tcp_priv_port_631_out': 0, 'tshark_tcp_priv_port_137_out': 0, 'tshark_tcp_priv_port_138_out': 0, 'tshark_tcp_priv_port_139_out': 0, 'tshark_tcp_priv_port_110_out': 0, 'tshark_tcp_priv_port_143_out': 0, 'tshark_tcp_priv_port_80_out': 1, 'tshark_tcp_priv_port_53_out': 0, 'tshark_tcp_priv_port_22_out': 0, 'tshark_tcp_priv_port_23_out': 0, 'tshark_tcp_priv_port_88_out': 0, 'tshark_tcp_priv_port_25_out': 1, 'tshark_tcp_priv_port_443_out': 0, 'tshark_tcp_priv_port_123_out': 0, 'tshark_tcp_priv_port_other_out': 0}]
 
 
-def test_udp_priv_ports():
+def test_host_udp_priv_ports():
     instance = Host()
     rows = lambda: [{'eth.src': TEST_MAC, 'udp.srcport': 1025, 'udp.dstport': 123}]
     assert instance.host_tshark_priv_udp_ports_in(rows) == [
@@ -152,7 +152,7 @@ def test_udp_priv_ports():
         {'host_key': TEST_MAC, 'tshark_udp_priv_port_161_out': 0, 'tshark_udp_priv_port_67_out': 0, 'tshark_udp_priv_port_68_out': 0, 'tshark_udp_priv_port_69_out': 0, 'tshark_udp_priv_port_631_out': 0, 'tshark_udp_priv_port_137_out': 0, 'tshark_udp_priv_port_138_out': 0, 'tshark_udp_priv_port_139_out': 0, 'tshark_udp_priv_port_110_out': 0, 'tshark_udp_priv_port_143_out': 0, 'tshark_udp_priv_port_80_out': 0, 'tshark_udp_priv_port_53_out': 0, 'tshark_udp_priv_port_22_out': 0, 'tshark_udp_priv_port_23_out': 0, 'tshark_udp_priv_port_88_out': 0, 'tshark_udp_priv_port_25_out': 0, 'tshark_udp_priv_port_443_out': 0, 'tshark_udp_priv_port_123_out': 1, 'tshark_udp_priv_port_other_out': 0}]
 
 
-def test_tcp_nonpriv_ports():
+def test_host_tcp_nonpriv_ports():
     instance = Host()
     rows = lambda: [{'eth.src': TEST_MAC, 'tcp.srcport': 1025, 'tcp.dstport': 9999}]
     assert instance.host_tshark_nonpriv_tcp_ports_in(rows) == [
@@ -161,7 +161,7 @@ def test_tcp_nonpriv_ports():
         {'host_key': TEST_MAC, 'tshark_tcp_nonpriv_port_5349_out': 0, 'tshark_tcp_nonpriv_port_5222_out': 0, 'tshark_tcp_nonpriv_port_2375_out': 0, 'tshark_tcp_nonpriv_port_2376_out': 0, 'tshark_tcp_nonpriv_port_5353_out': 0, 'tshark_tcp_nonpriv_port_5354_out': 0, 'tshark_tcp_nonpriv_port_1900_out': 0, 'tshark_tcp_nonpriv_port_5357_out': 0, 'tshark_tcp_nonpriv_port_6653_out': 0, 'tshark_tcp_nonpriv_port_other_out': 1}]
 
 
-def test_wk_ip_protos():
+def test_host_wk_ip_protos():
     instance = Host()
     assert instance.host_tshark_wk_ip_protos(
         lambda: [{'eth.src': TEST_MAC, 'tcp.something': 'whatever', 'udp.something': 'whatever', 'something.else': 'something', 'data.something': 'whatever'}]) == [
