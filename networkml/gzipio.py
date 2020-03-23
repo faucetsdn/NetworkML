618b14d6cb15b9e107412b6803d4509ede408343
Josh Bailey <josh@vandervecken.com>
2020-03-24 11:10:48 +1300

    Text mode.

--- networkml/gzipio.py
+++ networkml/gzipio.py
@@ -2,8 +2,8 @@ import gzip
 import io
 
 def gzip_reader(gzip_file):
-    return io.TextIOWrapper(gzip.open(gzip_file, 'rb'), newline='')
+    return io.TextIOWrapper(gzip.open(gzip_file, 'rt'), newline='')
 
 
 def gzip_writer(gzip_file):
-    return io.TextIOWrapper(gzip.open(gzip_file, 'wb'), newline='', write_through=True)
+    return io.TextIOWrapper(gzip.open(gzip_file, 'wt'), newline='', write_through=True)
