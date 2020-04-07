9ebebdc08e3b79b3fdef37002202eb324529a216
Josh Bailey <josh@vandervecken.com>
2020-04-07 15:21:09 +1200

    Use smaller int types to save more memory.

--- networkml/pandas_csv_importer.py
+++ networkml/pandas_csv_importer.py
@@ -40,39 +40,39 @@ def _eth_protos(val):
 
 
 WS_FIELDS = {
-    'arp.opcode': (_safe_int, True),
+    'arp.opcode': (_safe_int, 8),
     'eth.src': (_netaddr_packed, None),
     'eth.dst': (_netaddr_packed, None),
-    'eth.type': (_hex_str, True),
-    'frame.len': (_safe_int, True),
-    'frame.time_epoch': (float, False),
-    'frame.time_delta_displayed': (float, False),
-    'frame.protocols': (_eth_protos, False),
-    'icmp.code': (_safe_int, True),
-    'gre.proto': (_hex_str, True),
+    'eth.type': (_hex_str, 16),
+    'frame.len': (_safe_int, 16),
+    'frame.time_epoch': (float, None),
+    'frame.time_delta_displayed': (float, None),
+    'frame.protocols': (_eth_protos, None),
+    'icmp.code': (_safe_int, 8),
+    'gre.proto': (_hex_str, 8),
     'ip.src': (_ipaddress_packed, None),
     'ip.src_host': (_ipaddress_packed, None),
     'ip.dst': (_ipaddress_packed, None),
     'ip.dst_host': (_ipaddress_packed, None),
-    'ip.dsfield': (_hex_str, True),
-    'ip.flags': (_hex_str, True),
-    'ip.proto': (_safe_int, True),
-    'ip.version': (_safe_int, True),
-    'icmpv6.code': (_safe_int, True),
+    'ip.dsfield': (_hex_str, 8),
+    'ip.flags': (_hex_str, 16),
+    'ip.proto': (_safe_int, 8),
+    'ip.version': (_safe_int, 8),
+    'icmpv6.code': (_safe_int, 8),
     'ipv6.src': (_ipaddress_packed, None),
     'ipv6.src_host': (_ipaddress_packed, None),
     'ipv6.dst': (_ipaddress_packed, None),
     'ipv6.dst_host': (_ipaddress_packed, None),
-    'tcp.flags': (_hex_str, True),
-    'tcp.srcport': (_safe_int, True),
-    'tcp.dstport': (_safe_int, True),
-    'udp.srcport': (_safe_int, True),
-    'udp.dstport': (_safe_int, True),
-    'vlan.etype': (_hex_str, True),
-    'vlan.id': (_safe_int, True),
+    'tcp.flags': (_hex_str, 16),
+    'tcp.srcport': (_safe_int, 16),
+    'tcp.dstport': (_safe_int, 16),
+    'udp.srcport': (_safe_int, 16),
+    'udp.dstport': (_safe_int, 16),
+    'vlan.etype': (_hex_str, 16),
+    'vlan.id': (_safe_int, 16),
 }
 _WS_FIELDS_CONVERTERS = {field: field_info[0] for field, field_info in WS_FIELDS.items()}
-_WS_FIELDS_NULLABLE_INT = {field for field, field_info in WS_FIELDS.items() if field_info[1]}
+_WS_FIELDS_NULLABLE_INT = {field: 'UInt%s' % field_info[1] for field, field_info in WS_FIELDS.items() if isinstance(field_info[1], int)}
 _REQUIRED_WS_FIELDS = {'eth.src', 'eth.dst', 'frame.len', 'frame.time_epoch', 'frame.time_delta_displayed'}
 
 
@@ -89,6 +89,6 @@ def import_csv(in_file):
     # TODO: when pandas allows read_csv to infer nullable ints, we can use less memory on import.
     # https://github.com/pandas-dev/pandas/issues/2631
     # For now convert to nullable int after import.
-    for col in _WS_FIELDS_NULLABLE_INT:
-        df[col] = df[col].astype('Int64')
+    for col, typestr in _WS_FIELDS_NULLABLE_INT.items():
+        df[col] = df[col].astype(typestr)
     return df
