8825f25341267c034d3bca5356974ac208575848
Josh Bailey <josh@vandervecken.com>
2020-04-21 20:18:05 +1200

    Only drop host_key if present.

--- networkml/algorithms/host_footprint.py
+++ networkml/algorithms/host_footprint.py
@@ -111,7 +111,8 @@ class HostFootprint():
 
         # Load data from host footprint .csv
         df = pd.read_csv(self.path)
-        df = df.drop(columns=['host_key'])
+        if 'host_key' in df.columns:
+            df = df.drop(columns=['host_key'])
         df = df.fillna(0)
 
         # Split dataframe into X (the input features or predictors)
@@ -178,7 +179,8 @@ class HostFootprint():
 
         # Load data from host footprint .csv
         df = pd.read_csv(self.path)
-        df = df.drop(columns=['host_key'])
+        if 'host_key' in df.columns:
+            df = df.drop(columns=['host_key'])
 
         # Split dataframe into X (the input features or predictors)
         # and y (the target or outcome or dependent variable)
