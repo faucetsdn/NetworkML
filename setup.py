4a9b63d97a73ff48af751dfc36f0d6a8b31394c3
cglewis <clewis@iqt.org>
2020-06-24 09:34:50 -0700

    fix multi-arch, update numpy

--- setup.py
+++ setup.py
@@ -1,12 +1,11 @@
 # WARNING about imp deprecation because of setuptools
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='networkml',
     version=open('VERSION', 'r').read().strip(),
     include_package_data=True,
-    packages=['networkml', 'networkml.algorithms', 'networkml.featurizers.funcs',
-              'networkml.helpers', 'networkml.parsers', 'networkml.featurizers'],
+    packages=find_packages(),
     install_requires=open('requirements.txt', 'r').read().splitlines(),
     scripts=['bin/networkml'],
     license='Apache License 2.0',
