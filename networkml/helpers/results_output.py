162ea4ca588526d073094270fa207a5cfbb28eb1
Josh Bailey <josh@vandervecken.com>
2020-12-18 07:15:08 +0000

    Use new RBQ wrapper.

--- networkml/helpers/results_output.py
+++ networkml/helpers/results_output.py
@@ -1,49 +1,16 @@
 import json
 import os
 import re
-import socket
-
-import pika
 
 import networkml
 
 
 class ResultsOutput:
 
-    def __init__(self, logger, version, use_rabbit):
+    def __init__(self, logger, uid, file_path):
         self.logger = logger
-        self.version = version
-        self.use_rabbit = use_rabbit
-        self.rabbit_host = os.getenv('RABBIT_HOST', 'messenger')
-        self.rabbit_queue_name = os.getenv('RABBIT_QUEUE_NAME', 'task_queue')
-        self.rabbit_exchange = os.getenv('RABBIT_EXCHANGE', 'task_queue')
-        self.rabbit_port = int(os.getenv('RABBIT_PORT', '5672'))
-        self.rabbit_routing_key = os.getenv('RABBIT_ROUTING_KEY', 'task_queue')
-
-    def connect_rabbit(self):
-        params = pika.ConnectionParameters(host=self.rabbit_host, port=self.rabbit_port)
-        connection = pika.BlockingConnection(params)
-        channel = connection.channel()
-        channel.queue_declare(queue=self.rabbit_queue_name, durable=True)
-        return (connection, channel)
-
-    def send_rabbit_msg(self, msg, channel):
-        body = json.dumps(msg)
-        channel.basic_publish(exchange=self.rabbit_exchange,
-                              routing_key=self.rabbit_routing_key,
-                              body=body,
-                              properties=pika.BasicProperties(delivery_mode=2))
-        self.logger.info('send_rabbit_msg: %s', body)
-
-    def rabbit_msg_template(self, uid, file_path, result):
-        return {
-            'id': uid,
-            'type': 'metadata',
-            'file_path': file_path,
-            'data': result,
-            'results': {
-                'tool': 'networkml',
-                'version': self.version}}
+        self.uid = uid
+        self.file_path = file_path
 
     @staticmethod
     def assign_labels(labels):
@@ -84,36 +51,17 @@ class ResultsOutput:
             pcap_key = base_pcap.split('.')[0]
         return (pcap_key, pcap_labels)
 
-    def results_template(self, file_path, valid, results):
-        base_pcap = os.path.basename(file_path)
-        pcap_key, pcap_labels = self.parse_pcap_name(base_pcap)
-        base_results = {'valid': valid, 'pcap_labels': pcap_labels}
-        base_results.update(results)
-        return {pcap_key: base_results, 'pcap': base_pcap}
-
-    def output_msg(self, uid, file_path, result):
-        if not self.use_rabbit:
-            return
-
-        msg = result
-        try:
-            msg = self.rabbit_msg_template(uid, file_path, result)
-            (connection, channel) = self.connect_rabbit()
-            self.send_rabbit_msg(msg, channel)
-            msg = self.rabbit_msg_template(uid, file_path, '')
-            self.send_rabbit_msg(msg, channel)
-            connection.close()
-        except (socket.gaierror, pika.exceptions.AMQPConnectionError) as err:
-            self.logger.error(f'Failed to send Rabbit message {msg} because: {err}')
-
-    def output_invalid(self, uid, file_path, filename):
-        self.output_msg(
-            uid, file_path, self.results_template(filename, False, {}))
-
     @staticmethod
-    def valid_template(timestamp, source_ip, source_mac,
-                       investigate, labels, confidences):
+    def valid_template(uid, file_path, timestamp, source_ip, investigate, labels, confidences,
+                       pcap_labels, base_pcap, pcap_key):
         return {
+            'uid': uid,
+            'file_path': file_path,
+            'pcap': base_pcap,
+            'pcap_key': pcap_key,
+            'pcap_labels': pcap_labels,
+            'timestamp': timestamp,
+            'source_ip': source_ip,
             'decisions': {
                 'investigate': investigate,
             },
@@ -121,34 +69,33 @@ class ResultsOutput:
                 'labels': labels,
                 'confidences': confidences,
             },
-            'timestamp': timestamp,
-            'source_ip': source_ip,
-            'source_mac': source_mac,
         }
 
-    def output_valid(self, uid, file_path, filename, timestamp, source_ip, source_mac,
-                     labels, confidences,
-                     investigate=False):
-        labels = self.assign_labels(labels)
-        self.output_msg(uid, file_path, self.results_template(
-            filename, True, self.valid_template(
-                timestamp, source_ip, source_mac,
-                investigate, labels, confidences)))
+    def output_from_result_json(self, result_json, reformatted_result_json_file):
+        base_pcap = os.path.basename(self.file_path)
+        pcap_key, pcap_labels = self.parse_pcap_name(base_pcap)
 
-    def output_from_result_json(self, uid, file_path, result_json):
-        result = json.loads(result_json)
-        for filename, host_results in result.items():
+        mac_metadata = {}
+        for filename, host_results in result_json.items():
             filename = filename.split('.csv.gz')[0]
             for host_result in host_results:
                 top_role = host_result.get('top_role', None)
-                if top_role is None:
-                    self.output_invalid(uid, file_path, filename)
-                    continue
-                investigate = top_role == 'Unknown'
-                source_ip = host_result.get('source_ip', None)
-                source_mac = host_result.get('source_mac', None)
-                timestamp = host_result.get('timestamp', None)
-                labels, confidences = zip(*host_result['role_list'])
-                self.output_valid(
-                    uid, file_path, filename, timestamp, source_ip, source_mac,
-                    labels, confidences, investigate=investigate)
+                if top_role is not None:
+                    investigate = top_role == 'Unknown'
+                    source_ip = host_result.get('source_ip', None)
+                    source_mac = host_result.get('source_mac', None)
+                    timestamp = host_result.get('timestamp', None)
+                    labels, confidences = zip(*host_result['role_list'])
+                    labels = self.assign_labels(labels)
+                    mac_metadata[source_mac] = self.valid_template(
+                        self.uid, self.file_path, timestamp, source_ip,
+                        investigate, labels, confidences,
+                        pcap_labels, base_pcap, pcap_key)
+        reformatted_json = {
+            'tool': 'networkml',
+            'data': {
+                'mac_addresses': mac_metadata,
+            }}
+        with open(reformatted_result_json_file, 'w') as reformatted_result:
+            reformatted_result.write(json.dumps(reformatted_json))
+        return reformatted_json
