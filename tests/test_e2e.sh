d2d10fd641e86e161917f4a56c1b447292a58b60
Josh Bailey <josh@vandervecken.com>
2020-12-23 02:35:49 +0000

    run with wrapper.

--- tests/test_e2e.sh
+++ tests/test_e2e.sh
@@ -8,4 +8,7 @@ LABEL=$(jq < $TMPDIR/predict.json '.data.mac_addresses["00:04:00:81:81:d0"]["cla
 if [[ "$LABEL" == "" ]] ; then
     echo FAIL: no result from prediction
 fi
+TD=$(pwd)
+docker build -f Dockerfile . -t iqtlabs/networkml:latest
+docker run -i -e RESULT_PATH=/tmp/predict.json -v $TD/tests/test_data:/pcaps iqtlabs/networkml:latest /pcaps/trace_ab12_2001-01-01_02_03-client-ip-1-2-3-4.pcap -o/tmp
 echo PASS
