4ac05931a9f100394369c4108cfe6b7f4f3d083d
cglewis <clewis@iqt.org>
2021-05-20 13:15:10 -0700

    typo

--- tests/test_e2e.sh
+++ tests/test_e2e.sh
@@ -3,7 +3,7 @@
 set -e
 
 TMPDIR=$(mktemp -d)
-networkml ./tests/test_data/trace_ab12_2001-01-01_02_03-client-ip-1-2-3-4.pcap -o $TMPDIR --first_stage parser --final_stage algorithm --operation predic
+networkml ./tests/test_data/trace_ab12_2001-01-01_02_03-client-ip-1-2-3-4.pcap -o $TMPDIR --first_stage parser --final_stage algorithm --operation predict
 cat $TMPDIR/predict.json
 LABEL=$(jq < $TMPDIR/predict.json '[0].data.mac_addresses["00:04:00:81:81:d0"]["classification"]["labels"][0]')
 if [[ "$LABEL" == "" ]] ; then
